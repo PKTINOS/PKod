e89f4b96d77aa040ffef38dc2086aa5f2358bdc2
PKTINOS <parisoplop@gmail.com>
2017-01-27 23:47:05 +0200

    Added new commands to source

--- Program.vb
+++ Program.vb
@@ -1,7 +1,5 @@
 Module Module1
 
-   'PKod. An esoteric language made by PKTINOS
-
     Dim myOnlyValue As Double
     Dim codePos = 0
     Dim rand As New Random()
@@ -19,11 +17,20 @@ Module Module1
             Do While codePos <= code.Length - 1
                 If (codePos = code.Length - 1) Then
                     Action(code(codePos), Nothing)
+                Else
+                    Action(code(codePos), code(codePos + 1))
+                End If
+
+                codePos += 1
+                If codePos = code.Length Then
                     Exit Do
                 End If
-                Action(code(codePos), code(codePos + 1))
-                codePos += 1
             Loop
+
+            Console.WriteLine()
+            Console.WriteLine()
+            Console.WriteLine("All of the code was executed")
+            Console.WriteLine()
             Console.ReadLine()
             Console.Clear()
             Main()
@@ -32,6 +39,7 @@ Module Module1
         End Try
         
     End Sub
+
     Sub Action(ByVal c As Char, ByVal nextC As Char)
         Try
             'Note: When next char is used, it's skipped in the code
@@ -54,7 +62,7 @@ Module Module1
                 Return
             ElseIf (c = "p"c) Then
                 'Raises value to power of corresponding ascii char code of next char
-                myOnlyValue = Math.Pow(myOnlyValue, Asc(nextC))
+                myOnlyValue = Math.Pow(myOnlyValue, Integer.Parse(nextC))
                 codePos += 1
                 Return
             ElseIf (c = "n"c) Then
@@ -98,15 +106,15 @@ Module Module1
             ElseIf (c = "j"c) Then
                 'Take input of a char, and set it as value
                 myOnlyValue = Asc(Console.ReadKey().KeyChar)
-             ElseIf (c = "+"c) Then
+            ElseIf (c = "+"c) Then
                 'Adds 1 to the value
                 myOnlyValue += 1
             ElseIf (c = "-"c) Then
                 'Removes 1 from value
                 myOnlyValue -= 1
             ElseIf (c = "!") Then
-                'Sleep the thread for value * 1000 milliseconds
-                Threading.Thread.Sleep(Math.Round(myOnlyValue) * 1000)
+                'Sleep the thread for next char*1000
+                Threading.Thread.Sleep(Integer.Parse(nextC) * 1000)
             ElseIf (c = "z"c) Then
                 'Jump to start if next char isnt equal with value
                 If (Asc(nextC) <> Math.Round(myOnlyValue)) Then
@@ -114,10 +122,94 @@ Module Module1
                 Else
                     codePos += 1
                 End If
+            ElseIf (c = "u") Then
+                myOnlyValue = Math.PI
+            ElseIf (c = "b") Then
+                myOnlyValue = Math.E
+            ElseIf (c = "m") Then
+                myOnlyValue -= Asc(nextC)
+                codePos += 1
+                Return
+            ElseIf (c = "}") Then
+                myOnlyValue *= Integer.Parse(nextC)
+                codePos += 1
+                Return
+            ElseIf (c = "{") Then
+                myOnlyValue = Math.Log(myOnlyValue)
+                Return
+            ElseIf (c = ";") Then
+                myOnlyValue = Math.Log10(myOnlyValue)
+                Return
+            ElseIf (c = ":") Then
+                myOnlyValue = Math.Sqrt(myOnlyValue)
+                Return
+            ElseIf (c = "(") Then
+                'Skip next char if not prime
+                If isPrime(Math.Round(myOnlyValue)) = False Then
+                    codePos += 1
+                    Return
+                End If
+            ElseIf (c = ")") Then
+                'Raise e^value
+                myOnlyValue = Math.Exp(myOnlyValue)
+                Return
+            ElseIf (c = "$") Then
+                'Skip next char if even
+                If myOnlyValue Mod 2 = 0 Then
+                    codePos += 1
+                    Return
+                End If
+            ElseIf (c = "e") Then
+                'Multiply by 10^nextC's integer, if it exists
+                myOnlyValue *= Math.Pow(10, Integer.Parse(nextC))
+            ElseIf (c = "<") Then
+                'Go back to the start
+                codePos = -1
+            ElseIf (c = "?") Then
+                'Print a newline
+                Console.WriteLine()
+
+            ElseIf (c = "q") Then
+                myOnlyValue = Environment.TickCount
+            ElseIf (c = "~") Then
+                If (myOnlyValue Mod 2 = 0) Then
+                    Console.Write("True")
+                Else
+                    Console.Write("False")
+                End If
+            ElseIf (c = "x") Then
+                If (isPrime(Math.Round(myOnlyValue))) Then
+                    Console.Write("True")
+                Else
+                    Console.Write("False")
+                End If
+            ElseIf (c = ">") Then
+                codePos += 1
+                Return
+            ElseIf (c = "f") Then
+                myOnlyValue = Math.Abs(myOnlyValue)
             End If
-            End If
+
         Catch ex As Exception
             Console.WriteLine(ex.ToString())
         End Try
     End Sub
+    Public Function isPrime(ByVal number As Integer) As Boolean
+        If number = 1 Then
+            Return False
+        End If
+        If number = 2 Then
+            Return True
+        End If
+
+        Dim boundary = CInt(Math.Floor(Math.Sqrt(number)))
+
+        For i As Integer = 2 To boundary
+            If number Mod i = 0 Then
+                Return False
+            End If
+        Next
+
+        Return True
+    End Function
 End Module
